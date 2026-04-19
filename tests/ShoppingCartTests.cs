56eb180eb119f801b11f1f303ad24626ce6885b3
Fierclee <133076787+Fierclee@users.noreply.github.com>
2026-04-19 17:43:10 -0400

    Add LINQ using directive to ShoppingCartTests

---
+++ tests/ShoppingCartTests.cs
@@ -0,0 +1,39 @@
+using System.Linq;
+using Xunit;
+
+public class ShoppingCartTests
+{
+    [Fact]
+    public void AddItem_ShouldAddProduct()
+    {
+        var cart = new ShoppingCart(1);
+        var product = new Product(1, "Phone", "Smart phone", 500, "", "Electronics");
+
+        cart.addItem(product, 2);
+
+        Assert.Single(cart.items);
+    }
+
+    [Fact]
+    public void RemoveItem_ShouldRemoveProduct()
+    {
+        var cart = new ShoppingCart(1);
+        var product = new Product(1, "Phone", "Smart phone", 500, "", "Electronics");
+
+        cart.addItem(product, 2);
+        cart.removeItem(product);
+
+        Assert.Empty(cart.items);
+    }
+
+    [Fact]
+    public void GetTotal_ShouldCalculateCorrectly()
+    {
+        var cart = new ShoppingCart(1);
+        var product = new Product(1, "Phone", "Smart phone", 500, "", "Electronics");
+
+        cart.addItem(product, 2);
+
+        Assert.Equal(1000, cart.getTotal());
+    }
+}
